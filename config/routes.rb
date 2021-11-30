10256a46c593da2e53fabb12077c39decc8dc005
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-01 00:47:52 +0100

    feat : Created routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+    
+    resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
