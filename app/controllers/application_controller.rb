d6498dac8647b96c5762c10bf61840c193ebb58d
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-07 22:10:54 +0100

    fix: Fixed linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        User.take
-    end
+  def current_user
+    User.take
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,14 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'post_id', class_name: 'Like'
 
-
   def update_comments_counter
     comments_counter = Comment.where(post_id: id).count
-    self.update(comments_counter: comments_counter)
+    update(comments_counter: comments_counter)
   end
 
   def update_likes_counter
     likes_counter = Like.where(post_id: id).count
-    self.update(likes_counter: likes_counter)
+    update(likes_counter: likes_counter)
   end
 
   def most_recent_comments
