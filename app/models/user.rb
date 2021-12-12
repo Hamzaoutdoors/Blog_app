51b4f422da55473fbd5e21089e1acd8a89c5e81e
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-12 22:42:31 +0100

    Create User Model with specific methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
 
-  def most_recent_posts
+  def recent_posts
     posts = Post.all
     posts.order(created_at: :desc).limit(3)
   end
