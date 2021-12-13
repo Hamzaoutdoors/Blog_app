134383122636af06f2f7537fee76759fe049e748
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-14 00:08:42 +0100

    feat: Add actions to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: @user.id).order(:id)
 
-  def show; end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(@post.author_id)
+    @comments = Comment.where(post_id: @post.id)
+  end
 end
