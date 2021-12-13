dd09906d62573468c3aae76a22b34d3ffd1d8c88
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-14 00:09:15 +0100

    feat: Add actions to users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.order(:id)
 
-  def show; end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: @user.id)
+  end
+
+  def created 
+    @user = User.create(user_params)
+    redirect_to @user
+  end
+
+  private
+
+def user_params
+  params.require(:user).permit(:name, :bio, :photo)
+end
 end
