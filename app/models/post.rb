b9937921df1060f55ac8a293108583ebdd915910
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-15 18:30:31 +0100

    feat : Modify models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,11 @@
 class Post < ApplicationRecord
-  belongs_to :editor, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'post_id', class_name: 'Like'
+  belongs_to :user, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'post_id', class_name: 'Comment', dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', class_name: 'Like', dependent: :destroy
+  after_save :update_posts_count
 
-  def update_comments_counter
-    comments_counter = Comment.where(post_id: id).count
-    update(comments_counter: comments_counter)
-  end
-
-  def update_likes_counter
-    likes_counter = Like.where(post_id: id).count
-    update(likes_counter: likes_counter)
+  def update_posts_count
+    user.increment!(:posts_counter)
   end
 
   def recent_comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+ 
 
-  def recent_posts
-    posts = Post.all
-    posts.order(created_at: :desc).limit(3)
-  end
-
-  def update_posts_counter
-    posts_counter = Post.where(author_id: id).count
-    update(posts_counter: posts_counter)
+  def most_recent_posts
+    Post.where(author_id: id).last(3)
   end
 end
