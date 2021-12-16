9669506ebcdcf4c48841ac359eea5edb4e0466ed
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-16 19:10:54 +0100

    feat : Create form to add new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    comment = post.comments.create(comment_params.merge(user_id: current_user.id))
-    respond_with post, comment
+    @post = Post.find_by_id(params[:post_id])
+    @post.comments.create(comment_params.merge(author_id: current_user.id))
+    redirect_back(fallback_location: root_path)
+  end
+
+  private 
+
+  def comment_params
+    params.require(:data).permit(:text)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def flash_class(level)
+    bootstrap_alert_class = {
+      'success' => 'alert-success',
+      'error' => 'alert-danger',
+      'notice' => 'alert-info',
+      'alert' => 'alert-danger',
+      'warn' => 'alert-warning'
+    }
+    bootstrap_alert_class[level]
+  end
 end
