da4b841d16e1d04fc19685ad336be1fb42911144
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-16 19:11:07 +0100

    feat : Create form to add new like :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'comments/create'
-  resources :users
   root 'users#index'
     
-    resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 end
