ef123ef58f5185c319c138f01759dd9485e1399d
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-17 19:55:35 +0100

    test: Add Request specs for models and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
+
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Comment, type: :model do
+
+  before :each do
+    User.create(id: 1, name: 'hamza', posts_counter: 0)
+  end
+
+  before :each do
+    Post.create(id: 1, title: "Anything", text: "Lorem ipsum", comments_counter: 0, likes_counter: 0, author_id: 1)
+  end
+    subject do
+      Comment.new(author_id: 1, post_id: 1, text: "hahah you are crazy")
+    end
+  before { subject.save }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a text" do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'checks that "author_id" is an integer' do
+    subject.author_id = 7.5
+    expect(subject).to_not be_valid
+  end
+
+  it 'checks that "post_id" is an integer' do
+    subject.post_id = 7.5
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    User.new(name: "Anything", bio: "Lorem ipsum", photo: "https://ui-avatars.com/api/?name=sara&background=random", posts_counter: 1)
+  }
+  before { subject.save }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with posts_counter negative" do
+    subject.posts_counter = -5
+    expect(subject).to_not be_valid
+  end
+
+
+  it "is not valid with posts_counter not integer" do
+    subject.posts_counter = 7.7
+    expect(subject).to_not be_valid
+  end
 end
