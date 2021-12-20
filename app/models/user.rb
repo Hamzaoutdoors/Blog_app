ac6917578e231392c94b55728c32ae94956baaa9
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-20 21:51:47 +0100

    feat: Generate devise routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id, class_name: 'Post'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
