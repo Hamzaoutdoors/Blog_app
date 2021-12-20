f289c1a164a984efbf12749e488a98fd9d7e12c9
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-20 19:41:08 -0300

    feature: change add new user form to match user model constraints

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.2'
+ruby '3.0.3'
 
 gem 'jbuilder', '~> 2.7'
 gem 'pg', '~> 1.1'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
-
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :posts_counter, :email, :password])
   end
 end
