5d0620e21d88ff76d48c79b0888caca4f138ef21
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-21 20:17:19 -0300

    feature: update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-=begin users = User.create([
-	{ name: 'Nelsino', bio: "I'm the last oracle", posts_counter: 0},
-	{ name: 'Hamza', bio: "I'm the First oracle", posts_counter: 0},
-	{ name: 'Juan', bio: "I'm the Best oracle", posts_counter: 0},
-	{ name: 'Mateo', bio: "I'm the Crazy oracle", posts_counter: 0},
-	{ name: 'Laylo', bio: "I'm the sleepy oracle", posts_counter: 0}
+ admins = User.create([
+	{ name: 'Nelsino', bio: "I'm the last oracle", posts_counter: 0, email:'foo1@foo.com', password: 'admin123'},
+	{ name: 'Hamza', bio: "I'm the First oracle", posts_counter: 0, email:'foo2@foo.com', password: 'admin123'},
+	{ name: 'Herbert', bio: "I'm the Best oracle", posts_counter: 0, email:'foo3@foo.com', password: 'admin123'},
+
 	])
   
   all_users = User.all
@@ -17,8 +16,8 @@
   all_posts = Post.all
   
   all_posts.each do |post|
-	for j in 1..3 do
+	for j in 0..2 do
 	  Like.create(author_id: all_users[j].id, post_id: post.id)
 	  Comment.create(author_id: all_users[j].id, post_id: post.id, text: "I'm #{all_users[j].name} and I'm commenting gibberish here.")
 	end
-  end =end
+  end 
