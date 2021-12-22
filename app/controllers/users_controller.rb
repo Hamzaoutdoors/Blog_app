23e96d08c81e5521a2fadf7ac935b64025b844ac
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-22 19:19:13 -0300

    feature: display messages and check if user is saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to @user
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Signed in successfully'
+      redirect_to user_post_path(@post.author_id, @post.id)
+      redirect_to @user
+    end
   end
 
   def edit
@@ -21,5 +25,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :bio, :photo, :posts_counter, :email, :password)
+  end
+
+  def display_error_message
+    flash.now[:danger] = 'Form contains errors. Please see fields marked in red'
   end
 end
