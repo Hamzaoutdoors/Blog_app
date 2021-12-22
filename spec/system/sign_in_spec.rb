3b40fd1380ff70136e87258022c053d4c16879ae
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-22 19:19:41 -0300

    feature: add tests for sign_in with positive condition and negative condition

---
+++ spec/system/sign_in_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe "the signin process", type: :feature do
+  context "when successfull" do
+    before :each do
+      user = User.new(name: 'Nelsino', bio: "I'm the last oracle", posts_counter: 0, email: 'user@example.com')
+      user.password = 'admin123'
+      user.password_confirmation = 'admin123'
+      user.save
+    end
+
+    it "Log in => Signed in successfully." do
+      visit new_user_session_path
+      within("form") do
+        fill_in 'user_email', with: 'user@example.com'
+        fill_in 'user_password', with: 'admin123'
+      end
+      click_button 'Log in'
+      expect(page).to have_content 'Welcome to your Blog App'
+      expect(page).to have_content 'Signed in successfully.'
+      expect(page).to have_content 'Nelsino'
+    end
+  end
+  
+
+  
+
+  describe 'when unsuccesfull' do
+    before :each do
+      user = User.new(name: 'Nelsino', bio: "I'm the last oracle", posts_counter: 0, email: 'user@example.com')
+      user.password = 'admin123'
+      user.password_confirmation = 'admin123'
+      user.save
+    end
+
+    it "when email and passoword are not found" do
+      visit '/users/sign_in'
+      within("form") do
+        fill_in 'user_email', with: ''
+        fill_in 'user_password', with: ''
+      end
+      click_button 'Log in'
+
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+
+    it "when email and passoword are not found" do
+      visit '/users/sign_in'
+      within("form") do
+        fill_in 'user_email', with: ''
+        fill_in 'user_password', with: ''
+      end
+      click_button 'Log in'
+
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+
+    it "when email and passoword are not found" do
+      visit '/users/sign_in'
+      within("form") do
+        fill_in 'user_email', with: ''
+        fill_in 'user_password', with: ''
+      end
+      click_button 'Log in'
+
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+
+  end
+end
