c46984083b791956a180316566fa87b27ef12a3d
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-12-25 00:50:39 +0100

    feat: Add APP endpoints to posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,7 @@ class API::PostsController < ApplicationController
     load_and_authorize_resource
 
     def index
-        @posts = Post.all.order('created_at')
-        render json: @posts
+        @posts = Post.all.order(:created_at)
+        render json: { success: true, data: { posts: @posts } }
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :posts, foreign_key: :author_id, class_name: 'Post'
